d2169446df9aa06f903099f0a902713257cde3bd
jawadraza8372 <jawadraza8372@gmail.com>
2024-07-05 18:45:18 +0500

    pushed

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
-import '@firebase/firestore';
-
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  apiKey: "AIzaSyCL8sEBFJ6XAH2fppW2UYWbGPizgQtiu8A",
   authDomain: "coinmath-536d9.firebaseapp.com",
   projectId: "coinmath-536d9",
   storageBucket: "coinmath-536d9.appspot.com",
   messagingSenderId: "110498776243",
   appId: "1:110498776243:web:8be7b7ae727d97a1051cc6",
-  measurementId: "G-BBVVS32JTC"
+  measurementId: "G-BBVVS32JTC",
 };
 
 const app = initializeApp(firebaseConfig);
